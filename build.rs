02a7aec4067c899a261b88acef067d9356cf207c
Efstratios P. <64653145+efstratios97@users.noreply.github.com>
2026-04-28 11:08:26 +0200

    Fix method api parameter creation (#8)
    According to MPL 2.0 contributing back to Source via Pull Request; Bug Fix Method API

--- build.rs
+++ build.rs
@@ -1,10 +1,6 @@
 use std::path::Path;
 use std::process::exit;
-use std::{
-    fs::{File, write},
-    io::BufReader,
-};
-
+use std::{ fs::{ File, write }, io::BufReader };
 use serde::de::DeserializeOwned;
 
 // Iclude property models to map the json
@@ -21,17 +17,17 @@ type ApiFilesResult<T> = Result<Vec<T>, Box<dyn std::error::Error>>;
 ///////////////////////////////////////////////////////////////////////////
 /// COMMON ////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
-
 const HEADER: &str = "
 // Copyright (c) 2026 Efstratios Pahis
 // SPDX-License-Identifier: MPL-2.0
 ";
 
-const COMMON_INIT_CODE: &str = "
+const COMMON_INIT_CODE: &str =
+    "
 #![allow(unused_variables)]
 #![allow(non_snake_case)]
 
-#[derive(serde::Deserialize, serde::Serialize)]
+#[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub struct APICallResponse {
     jsonrpc: String,
     result: String,
@@ -42,10 +38,7 @@ type APICallResult = Result<APICallResponse, Box<dyn std::error::Error>>;\n
 ";
 
 /// Read json by passed file_path
-fn read_json_file<T>(file_path: &str) -> ApiFilesResult<T>
-where
-    T: DeserializeOwned,
-{
+fn read_json_file<T>(file_path: &str) -> ApiFilesResult<T> where T: DeserializeOwned {
     let file = File::open(file_path)?;
     let reader = BufReader::new(file);
     let json_file: Vec<T> = serde_json::from_reader(reader)?;
@@ -59,8 +52,8 @@ fn write_to_file(code: String, file_path: &str) {
     write(&dest_path, code).unwrap();
 }
 
-fn match_set_value_type(rpc_data: &Option<JsonRpcPayload>) -> Option<String> {
-    let params = rpc_data.as_ref().unwrap().params.get("value");
+fn match_set_value_type(rpc_data: &Option<JsonRpcPayload>, param_key: &str) -> Option<String> {
+    let params = rpc_data.as_ref().unwrap().params.get(param_key);
 
     if params.is_none() {
         None
@@ -71,9 +64,8 @@ fn match_set_value_type(rpc_data: &Option<JsonRpcPayload>) -> Option<String> {
             "<int>" => Some(String::from("u64")),
             "<object>" => Some(String::from("std::collections::HashMap<String, String>")),
             "[<string>]" => Some(String::from("std::vec::Vec<String>")),
-            "[<object>]" => Some(String::from(
-                "std::vec::Vec<std::collections::HashMap<String, String>>",
-            )),
+            "[<object>]" =>
+                Some(String::from("std::vec::Vec<std::collections::HashMap<String, String>>")),
             _ => Some(String::from("String")),
         }
     }
@@ -82,44 +74,38 @@ fn match_set_value_type(rpc_data: &Option<JsonRpcPayload>) -> Option<String> {
 ///////////////////////////////////////////////////////////////////////////
 /// Barco Property API ////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
-
 fn create_property_fns(properties: Vec<Property>) {
     // boilerplate
     let mut generated_code = String::from(HEADER);
     // init code
     generated_code.push_str(COMMON_INIT_CODE);
     // Iterate over all properties
-    properties.iter().for_each(|property| {
-        generated_code.push_str(&get_jsonrpc_generated_properties_code(property))
-    });
+    properties
+        .iter()
+        .for_each(|property| {
+            generated_code.push_str(&get_jsonrpc_generated_properties_code(property))
+        });
     write_to_file(generated_code, "./src/property_api.rs");
 }
 
 fn get_fn_signature_by_property_type(property: &Property) -> String {
-    let property_name = property
-        .property
-        .clone()
-        .replace(".", "_")
-        .replace("-", "_");
+    let property_name = property.property.clone().replace(".", "_").replace("-", "_");
     if property.access == "R" {
         String::from("get_") + &property_name + "(address: &str)"
     } else {
-        String::from("set_")
-            + &property_name
-            + format!(
+        String::from("set_") +
+            &property_name +
+            format!(
                 "(\n    address: &str,\n    value: {})",
-                match_set_value_type(&property.set).unwrap()
-            )
-            .as_str()
+                match_set_value_type(&property.set, "value").unwrap()
+            ).as_str()
     }
 }
 
 fn get_jsonrpc_generated_properties_code(property: &Property) -> String {
     // Helper func to replace id
     fn payload_id_helper(payload: &mut JsonRpcPayload, property: &Property) -> JsonRpcPayload {
-        payload.id = payload
-            .id
-            .replace("<number|string>", property.property.as_str());
+        payload.id = payload.id.replace("<number|string>", property.property.as_str());
         payload.clone()
     }
 
@@ -132,7 +118,7 @@ fn get_jsonrpc_generated_properties_code(property: &Property) -> String {
         payload_id_helper(&mut payload, &property);
 
         format!(
-        "///{}
+            "///{}
 pub async fn {} -> APICallResult {{
     let client = reqwest::Client::new();
     let payload = serde_json::json!({{\"jsonrpc\":\"{}\",\"method\":\"{}\",\"id\":\"{}\",\"params\":{{\"property\":\"{}\",\"value\":value}}}}).to_string();
@@ -141,13 +127,13 @@ pub async fn {} -> APICallResult {{
     let res: APICallResponse = serde_json::from_str(&res_body)?;
     Ok(res)
 }}\n",
-        &property.description,
-        fn_signature,
-        payload.jsonrpc,
-        payload.method,
-        payload.id,
-        payload.params.get("property").unwrap()
-    )
+            &property.description,
+            fn_signature,
+            payload.jsonrpc,
+            payload.method,
+            payload.id,
+            payload.params.get("property").unwrap()
+        )
     } else {
         format!(
             "///{}
@@ -168,7 +154,6 @@ pub async fn {} -> APICallResult {{
 ///////////////////////////////////////////////////////////////////////////
 /// Barco Method API //////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
-
 fn create_method_fns(methods: Vec<Method>) {
     // boilerplate
     let mut generated_code = String::from(HEADER);
@@ -181,43 +166,78 @@ fn create_method_fns(methods: Vec<Method>) {
     write_to_file(generated_code, "./src/method_api.rs");
 }
 
-fn get_fn_signature_by_method_type(method: &Method) -> String {
-    let method_name = method
-        .method
-        .clone()
-        .replace(".", "_")
-        .replace("-", "_")
-        .replace(" ", "");
+fn get_fn_signature_by_method_type(method: &Method) -> (String, Option<String>) {
+    let method_name = method.method.clone().replace(".", "_").replace("-", "_").replace(" ", "");
 
-    let param_value = match_set_value_type(&Some(method.request.clone()));
+    let mut param_signature: Option<String> = None;
+    let mut param_signature_tmp = String::from("");
 
-    if param_value.is_some() {
-        String::from("set_")
-            + &method_name
-            + format!(
-                "(\n    address: &str,\n    value: {})",
-                param_value.unwrap()
-            )
-            .as_str()
+    let mut body_params: Option<String> = None;
+    let mut body_params_tmp = String::from("");
+
+    method.request.params.iter().for_each(|(param_key, _)| {
+        let param_key = if param_key == "type" { "r#type" } else { param_key };
+        // Common
+        let param_value_type = match_set_value_type(&Some(method.request.clone()), param_key);
+        if param_value_type.is_some() {
+            // Signature
+            param_signature_tmp += &format!("{}: {},", param_key, param_value_type.unwrap());
+            // Body
+            body_params_tmp += &format!("\"{}\": {},", param_key, param_key);
+        }
+        // Signature
+        param_signature = Some(param_signature_tmp.clone());
+        // Body
+        body_params = Some(body_params_tmp.clone());
+    });
+
+    if param_signature.is_some() {
+        (
+            String::from("set_") +
+                &method_name +
+                format!("(\n    address: &str,\n    {})", param_signature.unwrap()).as_str(),
+            body_params,
+        )
     } else {
-        String::from("set_") + &method_name + "(address: &str)"
+        (String::from("set_") + &method_name + "(address: &str)", body_params)
     }
 }
 
 fn get_jsonrpc_generated_methods_code(method: &Method) -> String {
     // Helper func to replace id
     fn payload_id_helper(payload: &mut JsonRpcPayload, method: &Method) -> JsonRpcPayload {
-        payload.id = payload
-            .id
-            .replace("<number|string>", method.method.as_str());
+        payload.id = payload.id.replace("<number|string>", method.method.as_str());
         payload.clone()
     }
 
     let mut payload = method.request.clone();
     payload_id_helper(&mut payload, &method);
 
-    format!(
-        "///{}
+    let fn_parts = get_fn_signature_by_method_type(&method);
+    let fn_signature = fn_parts.0.as_str();
+    let fn_body = fn_parts.1;
+
+    if fn_body.is_some() {
+        format!(
+            "///{}
+pub async fn {} -> APICallResult {{
+    let client = reqwest::Client::new();
+    let payload = serde_json::json!({{\"jsonrpc\":\"{}\",\"method\":\"{}\",\"id\":\"{}\",\"params\":{{{}}}}}).to_string();
+    let res = client.post(address).body(payload).send().await?;
+    let res_body = res.text().await?;
+    let res: APICallResponse = serde_json::from_str(&res_body)?;
+    Ok(res)
+}}\n",
+            &method.description,
+            fn_signature,
+            payload.jsonrpc,
+            payload.method,
+            payload.id,
+            fn_body.as_ref().unwrap()
+        )
+    } else {
+        format!(
+            "///{}
 pub async fn {} -> APICallResult {{
     let client = reqwest::Client::new();
     let res = client.post(address).body({:?}).send().await?;
@@ -225,10 +245,11 @@ pub async fn {} -> APICallResult {{
     let res: APICallResponse = serde_json::from_str(&body)?;
     Ok(res)
 }}\n",
-        &method.description,
-        get_fn_signature_by_method_type(&method).as_str(),
-        serde_json::to_string(&payload).unwrap()
-    )
+            &method.description,
+            fn_signature,
+            serde_json::to_string(&payload).unwrap()
+        )
+    }
 }
 
 fn main() {
